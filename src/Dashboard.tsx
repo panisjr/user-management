1f52e33dbeb86509fa22e9d68125b0aeeb1e85a7
ramz <ramelopanisjr.06@gmail.com>
2025-03-31 18:14:09 +0800

    Update: crud

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
 import DataTable from "./components/DataTable";
-import { AuthProps } from "./types";
-import { CiCirclePlus } from "react-icons/ci";
+import { AuthProps} from "./types";
+import { CiCirclePlus} from "react-icons/ci";
 import { RxCross2 } from "react-icons/rx";
 import Swal from "sweetalert2";
 
 const Dashboard: React.FC<AuthProps> = ({ users, setUsers }) => {
+  const [firstname, setFirstname] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [addModal, setAddModal] = useState<boolean>(false);
-
-  const addUser = (email: string, password: string) => {
+  const addUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     const foundUser = users.find((user) => user.email === email);
 
     if (foundUser) {
       Swal.fire({
         icon: "error",
         title: "Sign Up!",
-        text: "Username already exists.",
+        text: "Email already exists.",
       });
     } else if (email.trim() === "") {
       Swal.fire({
         icon: "error",
         title: "Signing Up!",
-        text: "Username field is empty.",
+        text: "Email field is empty.",
       });
     } else if (password.trim() === "") {
       Swal.fire({
@@ -36,22 +38,33 @@ const Dashboard: React.FC<AuthProps> = ({ users, setUsers }) => {
         icon: "success",
         title: "Signed up successfully!",
       });
-
       setUsers([
         ...users,
-        { email, password, date: new Date().toISOString() },
+        {
+          email,
+          firstname,
+          lastname,
+          password,
+          date: new Date().toISOString(),
+        },
       ]);
+      setFirstname("");
+      setLastname("");
       setEmail("");
       setPassword("");
     }
   };
+
   const closeAddModal = () => {
     setAddModal(false);
+    setFirstname("");
+    setPassword("");
     setEmail("");
     setPassword("");
   };
+
   return (
-    <div>
+    <>
       <div className="flex items-center p-5">
         <button
           className="gap-3 border text-white bg-blue-600 px-3 py-2 rounded-md cursor-pointer hover:bg-white hover:text-black duration-300 flex item-center"
@@ -61,6 +74,8 @@ const Dashboard: React.FC<AuthProps> = ({ users, setUsers }) => {
           <CiCirclePlus className="w-6 h-6" />
         </button>
       </div>
+   
+    
       {users ? (
         <>
           <DataTable users={users} setUsers={setUsers} />
@@ -71,63 +86,101 @@ const Dashboard: React.FC<AuthProps> = ({ users, setUsers }) => {
         </div>
       )}
       {addModal && (
-        <>
-          <div className="fixed inset-0 bg-black/30 flex flex-col items-center justify-center poppins-regular">
-            <div className="relative inset top-0 bg-white flex flex-col items-center justify-center border border-slate-300 shadow-lg shadow-slate-500 rounded-md p-10 gap-5">
-              <div className="w-full flex items-center justify-end">
-                <RxCross2
-                  className="w-5 h-5 cursor-pointer hover:text-red-500 duration-300"
-                  onClick={() => closeAddModal()}
-                />
-              </div>
-              <p className="font-bold text-2xl w-full flex items-center justify-center">
-                Add User
-              </p>
-
-              {/* Input Fields */}
-              <div className="flex flex-col items-center gap-5">
+        <div className="fixed inset-0 flex flex-col items-center justify-center bg-black/30">
+          {/* Input Fields */}
+          <form
+            onSubmit={addUser}
+            className="bg-white p-5 rounded-md border border-gray-300 shadow-lg"
+          >
+            <div className="w-full flex items-center justify-end">
+              <RxCross2
+                className="w-6 h-6 hover:text-red-500 cursor-pointer duration-300"
+                onClick={() => closeAddModal()}
+              />
+            </div>
+            <p className="font-bold text-2xl w-full flex items-center justify-center py-10">
+              Add Account
+            </p>
+            <div className="flex flex-col items-center gap-5">
+              <div className="flex gap-2">
+                {/* First Name */}
                 <div className="flex items-center gap-3">
-                  <div className="border-b px-3 py-2 flex items-center gap-3">
+                  <div className="border rounded-md px-3 py-2 flex items-center gap-3">
                     <input
                       type="text"
-                      name="email"
-                      id="email"
+                      name="firstname"
+                      id="firstname"
                       className="outline-none"
-                      placeholder="Username"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="First Name"
+                      value={firstname}
+                      onChange={(e) => setFirstname(e.target.value)}
+                      required
+                      autoComplete="off"
                     />
                   </div>
                 </div>
+                {/* Last Name */}
                 <div className="flex items-center gap-3">
-                  <div className="border-b px-3 py-2 flex items-center gap-3">
+                  <div className="border rounded-md px-3 py-2 flex items-center gap-3">
                     <input
-                      type="password"
-                      name="password"
-                      id="password"
+                      type="text"
+                      name="lastname"
+                      id="lastname"
                       className="outline-none"
-                      placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      placeholder="Last Name"
+                      value={lastname}
+                      onChange={(e) => setLastname(e.target.value)}
+                      required
+                      autoComplete="off"
                     />
                   </div>
                 </div>
               </div>
-
-              {/* Add User Button */}
-              <div className="w-full flex items-center justify-center">
-                <button
-                  className="bg-yellow-500 border border-yellow-500 w-full py-2 rounded-md cursor-pointer hover:bg-white duration-300"
-                  onClick={() => addUser(email.trim(), password.trim())}
-                >
-                  Add User
-                </button>
+              <div className="flex items-center gap-3 w-full">
+                <div className="w-full border rounded-md px-3 py-2 flex items-center gap-3">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="outline-none"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                    autoComplete="off"
+                  />
+                </div>
+              </div>
+              {/* Password */}
+              <div className="w-full flex items-center gap-3">
+                <div className="w-full border rounded-md px-3 py-2 flex items-center gap-3">
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="outline-none"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                    autoComplete="off"
+                  />
+                </div>
               </div>
             </div>
-          </div>
-        </>
+            {/* Sign Up Button */}
+            <div className="w-full flex items-center justify-center py-8">
+              <button
+                type="submit"
+                className="bg-yellow-500 border border-yellow-500 text-white w-full py-2 rounded-md cursor-pointer hover:text-yellow-500 hover:bg-white duration-300"
+              >
+                Add user
+              </button>
+            </div>
+          </form>
+        </div>
       )}
-    </div>
+    </>
   );
 };
 
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -1,18 +1,21 @@
 import { AuthProps, User } from "../types";
-import { CiEdit, CiTrash } from "react-icons/ci";
 import { useState } from "react";
 import Swal from "sweetalert2";
 import { RxCross2 } from "react-icons/rx";
+import { CiEdit, CiTrash } from "react-icons/ci";
+import { GoPencil } from "react-icons/go";
 
 const DataTable: React.FC<AuthProps> = ({ users, setUsers }) => {
-  const [newUsername, setNewUsername] = useState<string>("");
-  const [handleUser, setHandleUser] = useState<string>("");
   const [viewModal, setViewModal] = useState<boolean>(false);
+  const [firstname, setFirstname] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("");
   const [email, setEmail] = useState<string>("");
   const [date, setDate] = useState<string>("");
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [editingEmail, setEditingEmail] = useState<string | null>(null);
 
   // Delete User
-  const handleDeleteUser = (email: string) => {
+  const handleDeleteUser = () => {
     const findUser = users.find((a) => a.email === email);
     if (findUser) {
       Swal.fire({
@@ -27,8 +30,10 @@ const DataTable: React.FC<AuthProps> = ({ users, setUsers }) => {
         if (result.isConfirmed) {
           setUsers(users.filter((a) => a.email !== email));
 
-          setHandleUser("");
-          setNewUsername("");
+          setFirstname("");
+          setLastname("");
+          setEmail("");
+          setViewModal(false);
           Swal.fire({
             title: "Deleted!",
             text: "Your file has been deleted.",
@@ -38,13 +43,18 @@ const DataTable: React.FC<AuthProps> = ({ users, setUsers }) => {
       });
     }
   };
-  // Start editing a user
-  const handleEditUser = (email: string) => {
-    setHandleUser(email);
-    setNewUsername(email);
-  };
+
   // Update User
-  const handleUpdateUser = () => {
+  const handleUpdateUser = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    if (!editingEmail) {
+      Swal.fire({
+        title: "Error",
+        text: "No user selected for updating.",
+        icon: "error",
+      });
+      return;
+    }
     Swal.fire({
       title: "Are you sure?",
       icon: "warning",
@@ -56,14 +66,22 @@ const DataTable: React.FC<AuthProps> = ({ users, setUsers }) => {
       if (result.isConfirmed) {
         setUsers((prevUsers) =>
           prevUsers.map((user) =>
-            user.email === handleUser
-              ? { ...user, email: newUsername }
+            user.email === editingEmail
+              ? {
+                  ...user,
+                  firstname: firstname,
+                  lastname: lastname,
+                  email: email,
+                }
               : user
           )
         );
 
-        setHandleUser("");
-        setNewUsername("");
+        setFirstname("");
+        setLastname("");
+        setEmail("");
+        setViewModal(false);
+        setIsEditing(false);
         Swal.fire({
           title: "Update!",
           text: "Your file has been deleted.",
@@ -78,8 +96,11 @@ const DataTable: React.FC<AuthProps> = ({ users, setUsers }) => {
     const findUser = users.find((a) => a.email === email);
     if (findUser) {
       setViewModal(true);
+      setFirstname(findUser.firstname ?? "");
+      setLastname(findUser.lastname ?? "");
       setEmail(findUser.email);
       setDate(findUser.date);
+      setEditingEmail(findUser.email);
     }
   };
   // Close View User Modal
@@ -88,81 +109,181 @@ const DataTable: React.FC<AuthProps> = ({ users, setUsers }) => {
     setEmail("");
     setDate("");
   };
+  // Search function
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredItems, setFilteredItems] = useState<User[]>(users);
+  const searchItems = (searchInput: string) => {
+    setSearchTerm(searchInput);
+
+    if (!searchInput.trim()) {
+      setFilteredItems(users);
+    } else {
+      setFilteredItems(
+        users.filter(
+          (item) =>
+            item.firstname?.toLowerCase().includes(searchInput.toLowerCase()) ||
+            item.lastname?.toLowerCase().includes(searchInput.toLowerCase()) ||
+            item.email.toLowerCase().includes(searchInput.toLowerCase())
+        )
+      );
+    }
+  };
   return (
-    <div className="p-4 poppins-regular px-52">
+    <div className="p-4 poppins-regular px-52 poppins-regular">
+      <input
+        className="h-8 p-3 text-sm peer border border-gray-300 rounded-sm text-black focus:outline-none focus:ring-2 focus:ring-blue-400 mb-2"
+        value={searchTerm}
+        onChange={(e) => searchItems(e.target.value)}
+        placeholder="Search todo..."
+      />
       <table className="table-fixed w-full">
         <thead>
           <tr>
-            <th className="border border-black bg-gray-800 text-white">Email</th>
-            <th className="border border-black bg-gray-800 text-white">First Name</th>
-            <th className="border border-black bg-gray-800 text-white">Last Name</th>
-            <th className="border border-black bg-gray-800 text-white">Date Created</th>
+            <th className="border border-black bg-gray-800 text-white">
+              First Name
+            </th>
+            <th className="border border-black bg-gray-800 text-white">
+              Last Name
+            </th>
+            <th className="border border-black bg-gray-800 text-white">
+              Email
+            </th>
+            <th className="border border-black bg-gray-800 text-white">
+              Date Created
+            </th>
           </tr>
         </thead>
         <tbody>
-          {users &&
-            users.map((item, index) => (
-              <tr key={index} className="even:bg-gray-200" onClick={() => setViewModal(true)}>
-                <td className="border text-center">{item.email}</td>
-                <td className="border text-center">{item.firstname}</td>
-                <td className="border text-center">{item.lastname}</td>
-                <td className="border text-center">{item.date}</td>
-              </tr>
-            ))}
+          {filteredItems.length > 0 ? (
+            filteredItems.map((item, index) => {
+              // if (item.userID === userID) {
+              return (
+                <tr
+                  key={index}
+                  className="even:bg-gray-300 cursor-pointer hover:bg-black/30"
+                  onClick={() => viewUser(item.email)}
+                >
+                  <td className="border text-center">{item.firstname}</td>
+                  <td className="border text-center">{item.lastname}</td>
+                  <td className="border text-center">{item.email}</td>
+                  <td className="border text-center">{item.date}</td>
+                </tr>
+              );
+              // }
+            })
+          ) : (
+            <tr className="even:bg-gray-300 cursor-pointer hover:bg-black/30">
+              <i>No results found</i>
+            </tr>
+          )}
         </tbody>
       </table>
 
-      {/* Edit User */}
-      {handleUser && (
-        <div className="w-full flex items-center justify-center">
-          <div className="absolute flex flex-col items-center justify-between bg-white p-10 border border-gray-200 rounded-md shadow-lg shadow-gray-300">
-            <div className="border border-blue-900 rounded-md px-3 py-2 flex items-center gap-3">
-              <input
-                type="text"
-                name="newUsername"
-                id="newUsername"
-                className="outline-none"
-                value={newUsername}
-                onChange={(e) => setNewUsername(e.target.value)}
-              />
-            </div>
-            <div className="w-full flex items-center justify-end py-5 gap-5 text-white">
-              <button
-                className="px-5 py-2 rounded-md bg-green-400 cursor-pointer hover:bg-green-500 duration-300"
-                onClick={() => handleUpdateUser()}
-              >
-                Update
-              </button>
-              <button
-                className="px-5 py-2 rounded-md bg-red-400 cursor-pointer hover:bg-red-500 duration-300"
-                onClick={() => {
-                  setHandleUser("");
-                  setNewUsername("");
-                }}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
       {viewModal && (
-        <div className="fixed inset-0 bg-black/30 flex flex-col items-center justify-center poppins-regular">
-          <div className="relative inset top-0 p-5 bg-white shadow-md rounded-md">
+        <div className="w-full fixed inset-0 bg-black/30 flex flex-col items-center justify-center poppins-regular">
+          <div className="relative inset top-0 p-5 bg-white shadow-md rounded-md w-full max-w-110">
             <div className="w-full flex items-center justify-end">
               <RxCross2
                 className="w-5 h-5 cursor-pointer hover:text-red-500 duration-300"
                 onClick={() => closeAddModal()}
               />
             </div>
-            <span className="flex gap-3">
-              <label htmlFor="email">Username:</label>
-              <p>{email}</p>
-            </span>
-            <span className="flex gap-3">
-              <label htmlFor="date">Date Created:</label>
-              <p>{date}</p>
-            </span>
+            <div className="flex flex-col">
+              <div className="flex items-center gap-3">
+                <button
+                  className="border bg-yellow-500 flex items-center px-3 py-2 rounded-md text-white hover:bg-white hover:text-yellow-500 duration-300 gap-2 cursor-pointer"
+                  onClick={() => setIsEditing(!isEditing)}
+                >
+                  <p>Edit</p>
+                  <CiEdit className="w-5 h-5" />
+                </button>
+                <button
+                  className="border bg-red-500 flex items-center px-3 py-2 rounded-md text-white hover:bg-white hover:text-red-500 duration-300 gap-2 cursor-pointer"
+                  onClick={() => handleDeleteUser()}
+                >
+                  <p>Delete</p>
+                  <CiTrash className="w-5 h-5" />
+                </button>
+              </div>
+              <form onSubmit={handleUpdateUser}>
+                <span className="flex flex-col items-center gap-3">
+                  <label htmlFor="firstname" className="font-bold w-full">
+                    First Name:
+                  </label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    className="p-2 border rounded-md w-full"
+                    value={firstname}
+                    onChange={(e) => setFirstname(e.target.value)}
+                    disabled={!isEditing}
+                  />
+                  <label htmlFor="lastname" className="font-bold w-full">
+                    Last Name:
+                  </label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    className="p-2 border rounded-md w-full"
+                    value={lastname}
+                    onChange={(e) => setLastname(e.target.value)}
+                    disabled={!isEditing}
+                  />
+                </span>
+                <span className="flex flex-col items-center gap-3 pt-2">
+                  <label htmlFor="email" className="font-bold w-full">
+                    Email:
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="p-2 border rounded-md w-full"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    disabled={!isEditing}
+                  />
+                </span>
+                <span className="flex flex-col items-center gap-3 pt-2">
+                  <label htmlFor="date" className="font-bold w-full">
+                    Date Created:
+                  </label>
+                  <input
+                    type="text"
+                    name="date"
+                    id="date"
+                    className="p-2 border rounded-md w-full"
+                    value={date}
+                    onChange={(e) => setDate(e.target.value)}
+                    disabled={!isEditing}
+                  />
+                </span>
+                {isEditing ? (
+                  <div className="w-full flex items-center justify-end p-5 gap-2">
+                    <button
+                      type="button"
+                      className="bg-red-500 border border-red-500 px-4 py-2 text-white rounded-md cursor-pointer hover:bg-white hover:border-red-500 hover:text-red-500 duration-300"
+                      onClick={() => {
+                        closeAddModal(), setIsEditing(!isEditing);
+                      }}
+                    >
+                      Cancel
+                    </button>
+                    <button
+                      type="submit"
+                      className="bg-green-500 border border-green-500 px-4 py-2 text-white rounded-md cursor-pointer hover:bg-white hover:border-green-500 hover:text-green-500 duration-300"
+                    >
+                      Update
+                    </button>
+                  </div>
+                ) : (
+                  <div className="w-full flex items-center justify-end p-5"></div>
+                )}
+              </form>
+            </div>
+            <div></div>
           </div>
         </div>
       )}
